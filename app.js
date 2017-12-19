37b62a5b2096753c51c22879b34fbe3af1befa1b
cedrox <cedricf@outlook.com>
2017-12-19 17:38:41 +0100

    x frame option

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+
+module.exports = app;
